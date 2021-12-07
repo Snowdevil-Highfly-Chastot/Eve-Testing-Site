af5be313e268e612a2595fda4149d764ee04db3b
Snowdevil-Highfly-Chastot <mjetzer@live.com>
2021-12-07 14:59:47 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Eve-Testing_Site
+ A testing site which includes HTML, CSS, JavaScript, Bootstrap, and SCSS usage. This is a place to build and test prototype applications for Eve Online.
---
+++ js/sidebar.js
@@ -0,0 +1,4 @@
+window.onload = function setSidebarContent() {
+    let content = "This here is an awesome sidebar to demonstrate the capability of adding side-bar information to the website. <br> <br> Sidebars create an organized looking site when it come to delivering a ton of information"
+    document.getElementById("sidebar-dynamic-content").innerHTML = content;
+}
