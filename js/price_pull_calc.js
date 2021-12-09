5b9879f7594db8d2b37bd75bdcbfa8514ea807b4
Snowdevil-Highfly-Chastot <mjetzer@live.com>
2021-12-09 01:00:18 -0600

    Broke getData()

--- js/price_pull_calc.js
+++ js/price_pull_calc.js
@@ -6,7 +6,7 @@
     var result = [];
     var safe_id_set = [];
 
-    /*configuration section*/
+    /*Configuration section*/
 
     const service_url = "https://market.fuzzwork.co.uk/aggregates/?station=60003760&types=";
     const safe_item_limit = 200;
@@ -15,61 +15,68 @@
     var safe_item_index = 0;
     var fuzz_price_data = "";
 
-    /*Create data collection/distribution methods inside of an object*/
+    /*Create data collection/distribution methods*/
 
-    var subFunctions = {
-        getIds: function getFormListIds() {
-            type_ids = document.getElementById("type_id_list").value.split("\n");
-            if (type_ids == "") {
-                alert("ID list must be filled out");
-                return false;
+
+    function getIds() {
+        type_ids = document.getElementById("type_id_list").value.split("\n");
+        if (type_ids == "") {
+            alert("ID list must be filled out");
+            return false;
+        }
+        return this.type_ids;
+    }
+
+    async function getData(url) {
+        fetch(url)
+        .then(response => 
+         response.json())
+         console.log(response)
+        
+        .then((data) => {
+          console.log(data)
+          return data
+        });
+    }
+
+    function createTable(array) {
+        document.getElementById("result_container").style.display = '';
+        var table = document.getElementById('results');
+        for (var i = 0; i < array.length; i++) {
+            var row = document.createElement('tr');
+            for (var j = 0; j < array[i].length; j++) {
+                var cell = document.createElement('td');
+                cell.textContent = array[i][j];
+                row.appendChild(cell);
             }
-            return this.type_ids;
+            table.appendChild(row);
         }
-        ,
-        getData: function get(fuzz_price_data) {
-            var Httpreq = new XMLHttpRequest(); // a new request
-            Httpreq.open("GET", fuzz_price_data, false);
-            Httpreq.send(null);
-            return Httpreq.responseText;
-        }
-        ,
-        createTable: function tableCreation(array) {
-            document.getElementById("result_container").style.display = '';
-            var table = document.getElementById('results');
-            for (var i = 0; i < array.length; i++) {
-                var row = document.createElement('tr');
-                for (var j = 0; j < array[i].length; j++) {
-                    var cell = document.createElement('td');
-                    cell.textContent = array[i][j];
-                    row.appendChild(cell);
-                }
-                table.appendChild(row);
-            }
-            return table;
-        }
+        return table;
     }
 
     /*Collect ID's*/
 
-    subFunctions.getIds();
+    getIds();
 
     /*Return prices if under safe ID count*/
 
     if (type_ids.length < safe_item_limit) {
-        fuzz_price_data = JSON.parse(subFunctions.getData(service_url + type_ids.join(",")));
-        result = type_ids.map(function (type_id) { return [parseFloat(fuzz_price_data[type_id][order_type][order_level])]; });
-        subFunctions.createTable(result);
+
+        fuzz_price_data = getData(service_url + type_ids.join(","));
+        console.log(fuzz_price_data);
+        result = type_ids.map(function (type_id) { return [fuzz_price_data[type_id][order_type][order_level]]; });
+        createTable(result);
         console.log(result); //return
+
     } else {
 
         for (i = 0; i < type_ids.length; i++) {
-            // Copy itemss into a Safe Array
+            // Copy items into a Safe Array
             safe_id_set.push(type_ids[i]);
 
             //Once Full, Grab the data result
             if (safe_item_index > safe_item_limit) {
-                fuzz_price_data = JSON.parse(subFunctions.getData(service_url + safe_id_set.join(",")));
+                fuzz_price_data = JSON.parse(getData(service_url + safe_id_set.join(",")));
                 result = result.concat(safe_id_set.map(function (type_id) { return [parseFloat(fuzz_price_data[type_id][order_type][order_level])]; }));
 
                 //Reset the request buffer for the next set
@@ -81,10 +88,10 @@
 
         // Capture overflow buffer
         if (safe_id_set.length > 0) {
-            fuzz_price_data = JSON.parse(subFunctions.getData(service_url + safe_id_set.join(",")));
+            fuzz_price_data = JSON.parse(getData(service_url + safe_id_set.join(",")));
             result = result.concat(safe_id_set.map(function (type_id) { return [parseFloat(fuzz_price_data[type_id][order_type][order_level])]; }));
         }
-        subFunctions.createTable(result);
+        createTable(result);
         console.log(result); //return
     }
 }
